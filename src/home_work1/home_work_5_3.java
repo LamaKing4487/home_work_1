71874370ef848cdcbf743c29ac5e3947023ff335
LamaKing4487 <polina4487@mail.ru>
2021-12-07 20:09:03 +0300

    init

--- src/home_work1/home_work_5_3.java
+++ src/home_work1/home_work_5_3.java
@@ -7,5 +7,20 @@ public class home_work_5_3 {
         String str = scan.nextLine();
         String a = "Вася";
         String b = "Анастасия";
+        switch(str) {
+            case "Вася":
+                System.out.println("Привет!");
+                System.out.println("я тебя так долго ждал");
+                break;
+            case "Анастасия":
+                System.out.println("я тебя так долго ждал");
+                break;
+            default:
+                System.out.println("Добрый день, а вы кто?");
+                break;
+
+        }
+
     }
+
 }
