fd5ecc2154f2f9c36720d6a8e12307960d4a13c0
LamaKing4487 <polina4487@mail.ru>
2021-12-07 19:13:05 +0300

    init

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,17 +1,9 @@
 
 O
-src/home_work1/home_work_2.java,5\b\5b0968fe187a99484f01c87f8680569bce8a2e10
-O
-src/home_work1/home_work_3.java,b\e\be1626cb27d8afeddad9b5dc165e0ea5c34adfa0
-O
 src/home_work1/home_work_5.java,1\d\1dcb672933e2645b492634d4a7f7aefe02a6b625
 O
 src/home_work1/home_work_4.java,8\f\8fda7ae428d009ef3517db111ea971bdf5f632d3
 O
 src/home_work1/home_work_6.java,c\4\c427d33c2701cecbc4641100fa9868d7c01f08ff
-Q
-!src/home_work1/home_work_1_3.java,0\c\0c7338c919cc13fb3dc922009349ce5cfd864a10
-Q
-!src/home_work1/home_work_1_2.java,f\4\f410fd92ab6f6623ed163539fd23cddd0def31bb
 I
 src/com/company/Main.java,4\a\4a831b10439099d44774d0ccc387e808deffaeab
--- src/home_work1/home_work_2.java
+++ src/home_work1/home_work_2.java
@@ -6,10 +6,15 @@ public class home_work_2 {
         int b = 2;
         System.out.println(5 + 2 / a);
         System.out.println((5 + 2) / a);
-        System.out.println((5 + b++) / a);
-        System.out.println((5 + b++) / --a);
-        System.out.println((5 * 2 >> b++) / --a);
-        System.out.println((5 + 7 > 20 ? 68 : 22 * 2 >> b++) / --a);
-        System.out.println(6 - 2 > 3 && 12 * 12 <= 119);
+        int left = 5 + b++;
+        System.out.println((left) / a);
+        int left1 = 5 + b++;
+        System.out.println((left1) / --a);
+        int left2 = 5 * 2 >> b++;
+        System.out.println((left2) / --a);
+        int left3 = 5 + 7 > 20 ? 68 : 22 * 2;
+        int left4 = (left3 >> b++);
+        System.out.println(left4 / --a);
+
     }
 }
